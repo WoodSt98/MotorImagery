cfaefc63cf09e297585c744afff48f191ad90ff7
Josh <jsig885@gmail.com>
2024-11-14 19:43:40 -0600

    Transposing Data Successfully
    POGGERS

--- transpose_data.py
+++ transpose_data.py
@@ -4,32 +4,28 @@ import pandas as pd
 # take an argument in command line to specify the path to the JSON file
 import sys
 
+participant_num = sys.argv[1]
 
 # Function to extract data from a JSON structure
 def extract_data_from_json(json_file):
     with open(json_file, 'r') as file:
-        data = json.load(file)
+        data_og = json.load(file)
     
-    all_extracted_data = []
+    data = json.loads(data_og['data'])
+    return data
 
-    # Iterate through each hashcode and extract relevant data
-    for hashcode, details in data.items():
-        # Extract the 'data' field, which is a JSON string
-        extracted_list = json.loads(details['data'])
-        all_extracted_data.extend(extracted_list)
-
-    return all_extracted_data
 
 # Main function to read JSON, extract data, and save to CSV
 def main():
-    json_file_path = './data/data_participant_X.json'  # Path to your JSON file
-    output_csv_path = './data/participant_X.csv'
+    json_file_path = f'./data/participant_{participant_num}.json'  # Path to your JSON file
+    output_csv_path = f'./data/participant_{participant_num}.csv'
 
     # Extract data
     extracted_data = extract_data_from_json(json_file_path)
 
     # Convert to DataFrame
     df = pd.DataFrame(extracted_data)
+    df.drop(columns=['url', 'meta'], inplace=True)  # Drop unnecessary columns
 
     # Save to CSV
     df.to_csv(output_csv_path, index=False)
